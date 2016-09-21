e53aed7cef38a88a89d37cd34a8e15f8c7e22de5
esandine <esandine@stuy.edu>
2016-09-20 20:54:19 -0400

    One page works

--- venv/Pages.py
+++ venv/Pages.py
@@ -2,10 +2,17 @@ from flask import Flask
 
 app=Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "hello"
+
+@app.route("/goodbye/")
+def goodbye():
+    return "goodbye"
+
+@app.route("/help/")
+def help():
+    return "help"
 
 if __name__=='__main__':
     app.run()
-
-@app.route("/")
-def hello_world():
-    return 'hello'
